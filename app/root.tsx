93f53be01952aca32e2f2a41bbe88c73e5a6a35f
rerelurelu <awaawapurin@gmail.com>
2024-02-10 17:34:23 +0900

    init: Initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,9 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import styleSheet from './tailwind.css';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(styleSheet ? [{ rel: 'stylesheet', href: styleSheet }] : []),
 ];
 
 export default function App() {
@@ -19,6 +12,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="./output.css" rel="stylesheet" />
         <Meta />
         <Links />
       </head>
